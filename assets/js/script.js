5bc3e7d54bf150f7eb95bb5f28e3e1b05493deb7
Sullivan <sullivan.fabre@gmail.com>
2020-01-24 11:15:06 +0100

    start of the game: possibility to put in tokens

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,37 @@ function initGame(time) {
     createGrid();
     isVictory = false;
     gameStatus = [[], [], [], [], [], [], []];
+
+    var listCase = document.querySelectorAll("[data-case]");
+    listCase.forEach(element => {
+      element.addEventListener("click", event => {
+        if (!isVictory) {
+          play(event);
+        }
+      });
+    });
   }, time);
+}
+
+function play(event) {
+  var parrentID = event.target.parentNode.id;
+  var id = parrentID.split("-")[1];
+
+  if (gameStatus[id].length < 6) {
+    gameStatus[id].push(currentPlayer);
+    displayResult(event, gameStatus[id]);
+    changePlayer();
+  }
+}
+
+function displayResult(event, currentColum) {
+  var y = event.target.parentNode;
+  var ally = y.children;
+  ally[6 - currentColum.length].classList.add("payer" + (currentPlayer + 1));
+}
+
+function changePlayer() {
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
 }
 
 initGame(0);

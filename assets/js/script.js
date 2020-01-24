e1333b254cd7df15d304d52ee057d7c78e008568
Valentin <valentin.brassart@pictime-groupe.com>
2020-01-24 14:29:04 +0100

    add IA who block win and can win but don t put token

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var currentPlayer = 0; // 1
 var checkWinIndice = 0;
 var isVictory = false;
 var isIaPlayer = false;
+var isIAGame = false;
 
 function createGrid() {
   var grid = "";
@@ -41,7 +42,6 @@ function initGame(time) {
 }
 
 function play(event) {
-  
   if(!isIaPlayer){
     var parrentID = event.target.parentNode.id;
     var id = parrentID.split("-")[1];
@@ -57,7 +57,31 @@ function play(event) {
 }
 
 function turnIa () {
-
+  var win = false;
+  for (let id = 0; id < 7 && win == false; id++) {
+    gameStatus[id].push(1);
+    win = checkWinIA(getCoordonate(gameStatus, id), 1);
+    if (!win) {
+      gameStatus[id].pop();
+    } else {
+      displayResultIA(id, gameStatus[id]);
+      checkWin(getCoordonate(gameStatus, id), 1);
+    }
+  }
+  if (!win){
+    for (let id = 0; id < 7 && win == false; id++) {
+      gameStatus[id].push(0);
+      win = checkWinIA(getCoordonate(gameStatus, id), 0);
+      gameStatus[id].pop();
+      if (win == true) {
+        console.log(win);
+        console.log(getCoordonate(gameStatus, id))
+        gameStatus[id].push(1);
+        displayResultIA(id, gameStatus[id]);
+      }
+    }
+  }
+  changePlayer();
 }
 
 function playGame (id) {
@@ -73,6 +97,11 @@ function displayResult(event, currentColum) {
   var y = event.target.parentNode;
   var ally = y.children;
   ally[6 - currentColum.length].classList.add("player" + (currentPlayer + 1));
+}
+
+function displayResultIA(id, currentColum) {
+  var listCase = document.querySelectorAll(".rowPlay");
+  listCase[id].children[6 - currentColum.length].classList.add("player" + 2);
 }
 
 function changePlayer() {
@@ -120,6 +149,45 @@ function checkWin([x, y], player) {
   }
 }
 
+function checkWinIA([x, y], player) {
+
+  // Horizontal
+  let count = 0;
+  for (let j = 0; j < 6; j++) {
+    count = this.gameStatus[x][j] == player ? count + 1 : 0;
+    if (count >= 4 && !isVictory) {
+        return true;
+    }
+  }
+  // Vertical
+  count = 0;
+  for (let i = 0; i < 7; i++) {
+    count = this.gameStatus[i][y] == player ? count + 1 : 0;
+    if (count >= 4 && !isVictory) {
+      return true;
+    }
+  }
+  // Diagonal
+  count = 0;
+  let shift = x - y;
+  for (let i = Math.max(shift, 0); i <= Math.min(6, 7 + shift); i++) {
+    count = this.gameStatus[i][i - shift] == player ? count + 1 : 0;
+    if (count >= 4 && !isVictory) {
+      return true;
+    }
+  }
+  // Anti-diagonal
+  count = 0;
+  shift = x + y;
+  for (let i = Math.max(shift - 7 + 1, 0); i < Math.min(6, shift + 1); i++) {
+    count = this.gameStatus[i][shift - i] == player ? count + 1 : 0;
+    if (count >= 4 && !isVictory) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function win(player) {
   alert('Le joueur ' + player + ' gagne');
   isVictory = true;
@@ -133,9 +201,13 @@ replay.addEventListener('click', event => {
 });
 
 ia.addEventListener('click', event => {
+  isIAGame = true;
+  isIaPlayer = true;
   initGame(0);
 });
 
 multi.addEventListener('click', event => {
+  isIAGame = false;
+  isIaPlayer = false;
   initGame(0);
 });
